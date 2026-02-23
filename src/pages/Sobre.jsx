3bdc32acf25798e852407750f5e89436b8d75c82
oliveirasouza <oliveirasouza00@gmail.com>
2026-02-23 18:56:25 -0300

    Correcoes dos Arquivos

--- src/pages/Sobre.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Sobre() {
-  return (
-    <section className="page">
-      <h1>Sobre Nós</h1>
-      <p>Somos especialistas em estilo moderno e clássico.</p>
-    </section>
-  );
-}
