071ba7f3c71db6afa49272ce00958f88a038e75a
oliveirasouza <oliveirasouza00@gmail.com>
2026-02-23 20:41:57 -0300

    ajuste vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/arthurbarberhomeestilo/',
+// })
+
+
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/arthurbarberhomeestilo/',
-})
+  base: "/", // ← MUITO IMPORTANTE
+});
